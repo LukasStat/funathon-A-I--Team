5f7e62fbe86030e9bdf49ec55069cc4456d36534
LukasStat <lukas.sramek@statistik.gv.at>
2026-05-27 13:34:31 +0000

    Aufgabe 1

---
+++ ml_nace_lukas.py
@@ -0,0 +1,61 @@
+#%%
+import pandas as pd
+import mlflow as ml
+from dotenv import load_dotenv
+import os
+import polars as pl
+print(os.getcwd())
+os.chdir("/home/onyxia/work/funathon-A-I--Team")
+load_dotenv(override=True)
+
+
+df = pl.read_parquet("https://minio.lab.sspcloud.fr/projet-formation/diffusion/funathon/2026/project2/generation_None_temp08.parquet")
+# %%
+n_classes = df['code'].n_unique()
+# %%
+df.columns
+df.head
+
+# %%
+# Use train_test_split from sklearn.model_selection
+#  to split the dataset into train, validation, and test sets (70% / 15% / 15%). Do not forget to choose a random_state. Separate the target y from the features X, and convert them to numpy arrays. You should obtain objects X_train,
+#  y_train, and so on.
+
+from sklearn.model_selection import train_test_split
+# %%
+train_df, tmp_df = train_test_split(df, test_size=0.30, random_state=42)
+val_df, test_df  = train_test_split(tmp_df, test_size=0.50, random_state=42)
+# %%
+
+X_train = train_df["label"].to_numpy()
+y_train = train_df["code"].to_numpy()
+
+X_val = val_df["label"].to_numpy()
+y_val = val_df["code"].to_numpy()
+
+X_test = test_df["label"].to_numpy()
+y_test = test_df["code"].to_numpy()
+
+
+print(f"Train: {len(train_df)} | Val: {len(val_df)} | Test: {len(test_df)}")
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+
+# %%
+le.fit(y_train)
+# %%
+alle = set(df["code"].unique())
+train = set(train_df["code"].unique())
+
+diff = train - alle
+
+# %%
+# Hashable strings to integer
+encoder = LabelEncoder()
+encoder.fit(train_df['code'].to_numpy())
+# %%
+from torchTextClassifiers.value_encoder import ValueEncoder 
+# %%
+value_encoder = ValueEncoder(label_encoder=encoder)
+# %%
