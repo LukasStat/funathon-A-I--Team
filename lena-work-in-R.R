625fdb6069637637c3c10fefe81a5bf3ff3a0a7a
lwitt <lena.wittmann@statistik.gv.at>
2026-05-28 07:05:15 +0000

    your message

---
+++ lena-work-in-R.R
@@ -0,0 +1,17 @@
+install.packages("reticulate")
+
+library(reticulate)
+use_python("~/work/funathon-A-I--Team/.venv/bin/python", required = TRUE)
+py_config()
+
+
+py_run_string("
+from dotenv import load_dotenv
+load_dotenv()
+")
+
+
+py_run_string("
+import os
+print(os.getenv('MLFLOW_TRACKING_URI'))
+")
